f625fdb94b13a5663b5c10f73df2795eca97af1a
Quentin <quentin.durand7@etu.univ-lorraine.fr>
2024-06-14 09:03:25 +0200

    Question 4 terminé

--- WebDirectory.core/app/app/Action/GetListEntreAction.php
+++ WebDirectory.core/app/app/Action/GetListEntreAction.php
@@ -5,16 +5,31 @@ namespace WebDirectory\app\Action;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use WebDirectory\core\domain\entites\Departement;
 use WebDirectory\core\domain\entites\Personne;
 
 class GetListEntreAction extends AbstractAction
 {
     public function __invoke(Request $rq, Response $rs, array $args): Response
     {
+        $queryParams = $rq->getQueryParams();
+        $selectedDepId = $queryParams['departement'] ?? null;
 
-        $entre = Personne::all();
+        if ($selectedDepId) {
+            $entres = Personne::whereHas('departements', function ($query) use ($selectedDepId) {
+                $query->where('id', $selectedDepId);
+            })->orderBy('nom')->get();
+        } else {
+            $entres = Personne::with('departements')->orderBy('nom')->get();
+        }
+
+        $departements = Departement::all();
 
         $view = Twig::fromRequest($rq);
-        return $view->render($rs, 'TwigListEntre.twig', ['entre' => $entre]);
+        return $view->render($rs, 'TwigListEntre.twig', [
+            'entres' => $entres,
+            'departements' => $departements,
+            'selectedDepId' => $selectedDepId,
+        ]);
     }
 }
