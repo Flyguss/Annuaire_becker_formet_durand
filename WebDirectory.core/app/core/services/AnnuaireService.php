b64efd4e65852b73d38c9b5ebbd82ec07de1ef1a
Romain Formet <romain.formet@gmail.com>
2024-06-14 08:35:50 +0200

    Ajout des classes de services

--- WebDirectory.core/app/core/services/AnnuaireService.php
+++ WebDirectory.core/app/core/services/AnnuaireService.php
@@ -5,4 +5,24 @@ namespace WebDirectory\core\services;
 class AnnuaireService implements AnnuaireServiceInterface
 {
 
+
+    public function getDepartments()
+    {
+        $sql = "SELECT id, nom FROM Département";
+        $result = $this->conn->query($sql);
+        $departments = [];
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $departments[] = $row;
+            }
+        }
+
+        return $departments;
+    }
+
+    public function createEntry(array $data)
+    {
+        // TODO: Implement createEntry() method.
+    }
 }
